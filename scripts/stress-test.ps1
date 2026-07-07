57588afe2fecbf10a4bdb8e9ee0ec85de61f07e9
Bel-test-QA <bel.test.qa@gmail.com>
2026-07-07 16:08:26 +0300

    Update stress-test.ps1

--- scripts/stress-test.ps1
+++ scripts/stress-test.ps1
@@ -1,4 +1,9 @@
+#!/usr/bin/env pwsh
 # Скрипт параллельного запуска 10 окон Newman для стресс-тестирования
+```powershell
+
 1..10 | ForEach-Object { 
     Start-Process cmd -ArgumentList "/c newman run collections/ultimate_api.postman_collection.json -e environments/qa_postman_environment.json --iteration-count 100" 
 }
+
+```
